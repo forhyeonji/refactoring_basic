6d51cca710a915074dd1b7a025b05424930807a0
forhyeonji <ghkdguswl3@gmail.com>
2023-05-15 23:18:13 +0900

    함수 인라인하기

--- ch6/6-2.js
+++ ch6/6-2.js
@@ -1,20 +1,14 @@
 // 예제 1
 export function rating(driver) {
-  return moreThanFiveLateDeliveries(driver) ? 2 : 1;
-}
-
-function moreThanFiveLateDeliveries(dvr) {
-  return dvr.numberOfLateDeliveries > 5;
+  // ✏️ 객체의 이름이 잘 정리되어 있고 특별한 검증이 필요하지 않으므로 인라인해주기
+  return driver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
 
 // 예제 2
 function reportLines(customer) {
-  const lines = [];
-  gatherCustomerData(lines, customer);
-  return lines;
-}
-
-function gatherCustomerData(out, customer) {
-  out.push(['name', customer.name]);
-  out.push(['location', customer.location]);
+  // ✏️ 특별히 하는 일이 없으므로 인라인
+  const result = [];
+  result.push(['name', customer.name]);
+  result.push(['location', customer.location]);
+  return result;
 }
