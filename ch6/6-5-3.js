e11ad1149ceebc239edf936742889145316095a4
forhyeonji <ghkdguswl3@gmail.com>
2023-05-15 23:40:56 +0900

    함수 선언 바꾸기

--- ch6/6-5-3.js
+++ ch6/6-5-3.js
@@ -1,3 +1,4 @@
-export function inNewEngland(aCustomer) {
-  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(aCustomer.address.state);
+export function inNewEngland(state) {
+  // ✏️ 함수안에서 꼭 필요한 데이터만 받는다. 재사용성 Up
+  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(state);
 }
