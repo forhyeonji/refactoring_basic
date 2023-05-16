5b82c549117b119ee321aa0093df7c2c86946139
forhyeonji <ghkdguswl3@gmail.com>
2023-05-16 23:18:16 +0900

    예제

--- ch6/6-11.js
+++ ch6/6-11.js
@@ -1,29 +1,46 @@
 export function priceOrder(product, quantity, shippingMethod) {
-  const basePrice = product.basePrice * quantity;
-  const discount =
+  const basePrice = calculateBasePrice(product, quantity);
+  const discount = calculateDiscountedPrice(product, quantity);
+  const shippingCost = calculateShippingCost(
+    basePrice,
+    quantity,
+    shippingMethod
+  );
+
+  return basePrice - discount + shippingCost;
+}
+
+function calculateBasePrice(product, quantity) {
+  return product.basePrice * quantity;
+}
+
+function calculateDiscountedPrice(product, quantity) {
+  return (
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
-    product.discountRate;
-  const shippingPerCase =
+    product.discountRate
+  );
+}
+
+function calculateShippingCost(basePrice, quantity, shippingMethod) {
+  const result =
     basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountedFee
       : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
+  return result * quantity;
 }
 
 // 사용 예:
 const product = {
   basePrice: 10,
   discountRate: 0.1,
-  discountThreshold: 10,
+  discountThreshold: 10
 };
 
 const shippingMethod = {
   discountThreshold: 20,
   feePerCase: 5,
-  discountedFee: 3,
+  discountedFee: 3
 };
 
 const price = priceOrder(product, 5, shippingMethod);
