07e0bc62a951e130f66d64d807b7dcfd15f11bcf
forhyeonji <ghkdguswl3@gmail.com>
2023-05-17 23:30:48 +0900

    레코드 캡슐화하기

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,4 +1,40 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+  #name;
+  #country;
+  constructor(data) {
+    this.#data = data;
+    this.#name = data.name;
+    this.#country = data.country;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  set name(value) {
+    this.#name = value;
+  }
+
+  get country() {
+    return this.#country;
+  }
+
+  set country(value) {
+    this.#country = value;
+  }
+
+  get rawData() {
+    return { name: this.name, country: this.country }; // set이 있을때
+    return { ...this.#data }; // set 없이 get 만 있을때
+  }
+}
+
+const organization = new Organization({
+  name: 'Acme Gooseberries',
+  country: 'GB'
+});
+
+const organization1 = new Organization('Acme Gooseberries', 'GB'); // 이러한 방식도 있다
 
 organization.name = 'Dream Coding';
 console.log(organization.name);
