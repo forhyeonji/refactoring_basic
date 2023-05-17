9ee14d9e48330f268673a4fa7bc4ae003a03f691
forhyeonji <ghkdguswl3@gmail.com>
2023-05-18 00:08:21 +0900

    기본형을 객체로 바꾸기// 클래스로 바꾸기

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -2,14 +2,44 @@ export class Order {
   constructor(data) {
     this.priority = data.priority;
   }
+
+  isHighPriority() {
+    return this.priority.higherThan(new Priority('normal'));
+  }
+}
+
+class Priority {
+  #value;
+  constructor(value) {
+    if (Priority.legalValues().includes(value)) {
+      this.#value = value;
+    } else {
+      // ✏️ : 생성자 안에서 Error를 던지는 것은 보안에 취약하다.
+      throw new Error(`${value}  is invalid for Priority`);
+    }
+  }
+
+  get index() {
+    return Priority.legalValues().indexOf(this.#value);
+  }
+
+  equals(other) {
+    return this.#index === other.index;
+  }
+
+  higherThan(other) {
+    return this.#index > other.index;
+  }
+
+  static legalValues() {
+    return ['low', 'normal', 'high', 'rush'];
+  }
 }
 
 const orders = [
-  new Order({ priority: 'normal' }),
-  new Order({ priority: 'high' }),
-  new Order({ priority: 'rush' }),
+  new Order(new Priority('normal')),
+  new Order(new Priority('high')),
+  new Order(new Priority('rush'))
 ];
 
-const highPriorityCount = orders.filter(
-  (o) => 'high' === o.priority || 'rush' === o.priority
-).length;
+const highPriorityCount = orders.filter((o) => o.isHighPriority).length;
