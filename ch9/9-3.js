91406627cfc2efc9afe24231deb6c5ee8b23a168
forhyeonji <ghkdguswl3@gmail.com>
2023-05-24 23:25:25 +0900

    파생 변수를 질의 함수로 바꾸기

--- ch9/9-3.js
+++ ch9/9-3.js
@@ -2,11 +2,14 @@
 class Order {
   // 다른 코드 있다고 가정
   get discountedTotal() {
-    return this._discountedTotal;
+    // ✏️ 2) 아래와 같이 get하는 순간에 계산해서 반영하기!
+    return this._basePrice - this._discount;
   }
+
   set discount(value) {
     const old = this._discount;
     this._discount = value;
+    // ✏️ 1) 이렇게 discount를 바꿨을 뿐인데 다른 필드의 값도 바뀌는건 💩
     this._discountedTotal += old - value;
   }
 }
@@ -15,10 +18,10 @@ class Order {
 class ProductionPlan {
   // 다른 코드 있다고 가정
   get production() {
-    return this._production;
+    return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
   }
+
   applyAdjustment(adjustment) {
     this._adjustments.push(adjustment);
-    this._production += adjustment.amount;
   }
 }
