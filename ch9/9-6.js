13101a205dd9a1b55e90e82bd2199cd0b59b4398
forhyeonji <ghkdguswl3@gmail.com>
2023-05-25 00:25:23 +0900

    매직 리터럴 바꾸기

--- ch9/9-6.js
+++ ch9/9-6.js
@@ -1,3 +1,6 @@
+const STANDARD_GRAVITY = 9.81;
+// ✏️ 1) 코드상에서 특정한 숫자가 나오면 상수화 시켜서 코드에 사용하면 숫자의 의미를 파악 할 수 있다.
+
 function potentialEnergy(mass, height) {
-  return mass * 9.81 * height;
+  return mass * STANDARD_GRAVITY * height;
 }
