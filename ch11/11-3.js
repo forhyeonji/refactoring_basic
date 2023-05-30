9c39f72f0ad38f82141b99996f75d30721e96e51
forhyeonji <ghkdguswl3@gmail.com>
2023-05-30 23:25:26 +0900

    플래그 인수 제거하기

--- ch11/11-3.js
+++ ch11/11-3.js
@@ -1,19 +1,21 @@
 // 예제 1
-function setDimension(name, value) {
-  if (name === 'height') {
-    this._height = value;
-    return;
-  }
-  if (name === 'width') {
-    this._width = value;
-    return;
-  }
+function setWidth(value){
+  this._width = value;
+}
+
+function setHeight(value){
+  this._height = value;
 }
 
 // 예제 2
 class Concert {
-  book(customer, isPremium) {}
+  regularBook(customer) {}
+  premiumBook(customer) {}
+  // ✏️ 정말정말 필요한 경우에만 boolaen 인자를 받는 private 함수를 만들어도 괜찮다.
+  // ex) 코드의 중복이 많을경우
+  #book(customer, isPremium){}
 }
 
 // 예제 3
-function setSwitch(on);
+function switchOn();
+function switchOff();
