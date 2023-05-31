0a99741f117ab5365e6ab44e0e52af4c90e6daa1
forhyeonji <ghkdguswl3@gmail.com>
2023-05-31 23:07:00 +0900

    수정된 값 반환하기

--- ch11/11-11.js
+++ ch11/11-11.js
@@ -1,13 +1,15 @@
 export function ascentVelocity(points, totalMinutes) {
+  // ✏️ : 외부의 변수를 update 하지말고 return을 통해 변수에 할당하자!
   function calculateAscent() {
+    let result = 0;
     for (let i = 1; i < points.length; i++) {
       const verticalChange = points[i].elevation - points[i - 1].elevation;
       totalAscent += verticalChange > 0 ? verticalChange : 0;
     }
+    return result;
   }
 
-  let totalAscent = 0;
-  calculateAscent([{ elevation: 10 }, { elevation: 20 }]);
+  let totalAscent = calculateAscent([{ elevation: 10 }, { elevation: 20 }]);
 
   return totalAscent / totalMinutes;
 }
