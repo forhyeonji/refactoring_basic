9cc93b823e579b5cfbe48339be9abb9928a3172f
forhyeonji <ghkdguswl3@gmail.com>
2023-05-31 23:03:35 +0900

    명령을 함수로 바꾸기

--- ch11/11-10.js
+++ ch11/11-10.js
@@ -1,13 +1,5 @@
-export class ChargeCalculator {
-  constructor(customer, usage, provider) {
-    this._customer = customer;
-    this._usage = usage;
-    this._provider = provider;
-  }
-  get baseCharge() {
-    return this._customer.baseRate * this._usage;
-  }
-  get charge() {
-    return this.baseCharge + this._provider.connectionCharge;
-  }
+// ✏️ : 단순히 주어진 데이터를 이용해서 계산만해야 한다면 class 보다는 함수로 바꾸기!
+function charge(customer, usage, provider) {
+  const baseCharge = customer.baseRate * usage;
+  return baseCharge + provider.connectionCharge;
 }
