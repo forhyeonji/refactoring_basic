ed7ed682a463d05c546a2ce54226706d8e5060a7
forhyeonji <ghkdguswl3@gmail.com>
2023-05-31 23:16:48 +0900

    예외를 사전확인으로 바꾸기

--- ch11/11-13.js
+++ ch11/11-13.js
@@ -1,14 +1,16 @@
 const values = [];
 function getValueForPeriod(periodNumber) {
   const value = values[periodNumber];
-  if (!value) {
-    throw new Error('value is undefined');
+  // ✏️ : 예상가능한 오류에 예외를 던지는건 남용이다!
+
+  if (periodNumber < 0 || periodNumber >= values.length) {
+    return 0;
   }
-  return value;
+
+  return values[periodNumber];
+
+  // 아래처럼 간단히 한줄로만 써도 됨
+  // return values[periodNumber] ?? 0;
 }
 
-try {
-  getValueForPeriod(-10);
-} catch (error) {
-  console.log('에러 발생!');
-}
+getValueForPeriod(-10);
