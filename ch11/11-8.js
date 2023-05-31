223330e67930f1b32d2e91a44aa97b06a40de366
forhyeonji <ghkdguswl3@gmail.com>
2023-05-31 22:53:19 +0900

    생성자를 팩터리 함수로 바꾸기 (생성자 캡슐화)

--- ch11/11-8.js
+++ ch11/11-8.js
@@ -1,4 +1,6 @@
 export class Employee {
+  // ✏️: 생성자를 private 하게 만들면 좋지만 js에는 그런 기능이 x. 밑의 코드는
+  // 생성자를 private 하게 만들어서 외부에서 사용못하게 했다고 가정함.
   constructor(name, typeCode) {
     this._name = name;
     this._typeCode = typeCode;
@@ -14,4 +16,20 @@ export class Employee {
   static get legalTypeCodes() {
     return { E: 'Engineer', M: 'Manager', S: 'Salesman' };
   }
+
+  // 아래는 생성자 함수를 잠근대신 사용할 수 있는 생성자함수 대신의 함수
+  static createEngineer(name) {
+    return new Employee(name, 'E');
+  }
+
+  static createSeniorEngineer(name) {
+    return new Employee(name, 'SE');
+  }
+
+  static createMarketer(name) {
+    return new Employee(name, 'M');
+  }
 }
+
+// ✏️ 사용법 (이제 이름만 전달하면 된다.)
+const employee = Employee.createEngineer('엘리');
