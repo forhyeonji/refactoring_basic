3896a86b279be80518338c854cc2999128903a91
forhyeonji <ghkdguswl3@gmail.com>
2023-06-01 23:28:27 +0900

    슈퍼클래스를 위임으로 바꾸기

--- ch12/12-11-1.js
+++ ch12/12-11-1.js
@@ -1,3 +1,11 @@
 class List {}
 
-class Stack extends List {}
+class Stack {
+  constructor() {
+    this.storage = new List();
+  }
+  // pop과 push는 list를 사용해서 api를 제공!
+  // 위임을 하게 되면 list의 원하는 기능만 제공할 수 있다!!
+  pop() {}
+  push() {}
+}
