4f449d5343ca825739dfa3db773875901985352c
forhyeonji <ghkdguswl3@gmail.com>
2023-06-02 00:40:25 +0900

    로우레벨 리팩토링

--- challenge/statement.js
+++ challenge/statement.js
@@ -1,56 +1,79 @@
 export function statement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
+      perf.audience
+    }석)\n`;
+  }
 
-    switch (play.type) {
+  result += `총액: ${usd(totalAmount() / 100)}\n`;
+  result += `적립 포인트: ${totalCredits()}점\n`;
+  return result;
+
+  function playFor(performance) {
+    return plays[performance.playID];
+  }
+
+  function creditsFor(performance) {
+    let result = 0;
+    result += Math.max(performance.audience - 30, 0);
+    if ('comedy' === playFor(performance).type) {
+      result += Math.floor(performance.audience / 5);
+    }
+    return result;
+  }
+
+  function amountFor(performance) {
+    let result = 0;
+
+    switch (playFor(performance).type) {
       case 'tragedy': // 비극
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
+        result = 40000;
+        if (performance.audience > 30) {
+          result += 1000 * (performance.audience - 30);
         }
         break;
       case 'comedy': // 희극
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
+        result = 30000;
+        if (performance.audience > 20) {
+          result += 10000 + 500 * (performance.audience - 20);
         }
-        thisAmount += 300 * perf.audience;
+        result += 300 * performance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
+        throw new Error(`알 수 없는 장르: ${playFor(performance).type}`);
     }
 
-    // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
-    // 청구 내역을 출력한다.
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
-    }석)\n`;
-    totalAmount += thisAmount;
+    return result;
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
-  return result;
+
+  function totalAmount() {
+    return invoice.performances.reduce((sum, p) => (sum += amountFor(p)), 0);
+  }
+
+  function totalCredits() {
+    return invoice.performances.reduce((sum, p) => (sum += creditsFor(p)), 0);
+  }
+}
+
+function usd(number) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+  }).format(number);
 }
 
 // 사용예:
 const playsJSON = {
   hamlet: { name: 'Hamlet', type: 'tragedy' },
   'as-like': { name: 'As You Like It', type: 'comedy' },
-  othello: { name: 'Othello', type: 'tragedy' },
+  othello: { name: 'Othello', type: 'tragedy' }
 };
 
 const invoicesJSON = [
@@ -59,18 +82,18 @@ const invoicesJSON = [
     performances: [
       {
         playID: 'hamlet',
-        audience: 55,
+        audience: 55
       },
       {
         playID: 'as-like',
-        audience: 35,
+        audience: 35
       },
       {
         playID: 'othello',
-        audience: 40,
-      },
-    ],
-  },
+        audience: 40
+      }
+    ]
+  }
 ];
 
 const result = statement(invoicesJSON[0], playsJSON);
